fba02fa475ca0964f96cec8006c7e7a6947562f8
Nannan Gu <396426364@qq.com>
2019-01-02 10:39:13 +0800

    new

--- src/leetcode/P56.java
+++ src/leetcode/P56.java
@@ -1,4 +1,58 @@
 package leetcode;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Given a collection of intervals, merge all overlapping intervals.
+
+ Example 1:
+
+ Input: [[1,3],[2,6],[8,10],[15,18]]
+ Output: [[1,6],[8,10],[15,18]]
+ Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+ Example 2:
+
+ Input: [[1,4],[4,5]]
+ Output: [[1,5]]
+ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ */
 public class P56 {
+    public List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals, new IntervalComparator());
+
+        LinkedList<Interval> merged = new LinkedList<Interval>();
+        for (Interval interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || merged.getLast().end < interval.start) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.getLast().end = Math.max(merged.getLast().end, interval.end);
+            }
+        }
+
+        return merged;
+    }
+
+
+    private class IntervalComparator implements Comparator<Interval> {
+        @Override
+        public int compare(Interval a, Interval b) {
+            return a.start < b.start ? -1 : a.start == b.start ? 0 : 1;
+        }
+    }
+
 }
+
+class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+ }
